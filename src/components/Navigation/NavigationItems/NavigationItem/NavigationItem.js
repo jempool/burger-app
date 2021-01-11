ba675aebc5162d1a756177f428c5a807604329f4
Jem Pool <jem.suarez@correounivalle.edu.co>
2021-01-11 08:23:00 -0500

    'Order' page added using navigation

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./NavigationItem.module.css";
 
 export default function NavigationItem(props) {
   return (
     <li className={classes.NavigationItem}>
-      <a href={props.link} className={props.active ? classes.active : null}>
+      <NavLink activeClassName={classes.active} to={props.link} exact>
         {props.children}
-      </a>
+      </NavLink>
     </li>
   );
 }
